d20bf102f69f3402e01a0f23a71e9007710b9a23
DELL <wschxida@gmail.com>
2020-12-10 17:28:53 +0800

    11.5 run_in_linux.sh

--- run_in_linux.sh
+++ run_in_linux.sh
@@ -1,3 +1,9 @@
+
+service=/home/kismanager/KIS/Fetch_Agent_Service
+log=/home/kismanager/KIS/Fetch_Agent_Service/log
+time=$(date "+%Y%m%d")
+
+cd $service
 pid=`ps -ef|grep "gunicorn -w 10 -b 127.0.0.1:5101 run:app"| grep -v "grep"|awk '{print $2}'`
 if [ "$pid" != "" ]
 then
@@ -5,8 +11,9 @@ then
         kill -9 ${pid}
 fi
 echo "starting now.."
-nohup gunicorn -w 10 -b 127.0.0.1:5101 run:app > ./log/myout.log 2>&1 &
+nohup /usr/local/bin/gunicorn -w 10 -b 127.0.0.1:5101 run:app >> $log/Fetch_Agent_Service_$time.log 2>&1 &
 pid=`ps -ef|grep "gunicorn -w 10 -b 127.0.0.1:5101 run:app"| grep -v "grep"|awk '{print $2}'`
-echo ${pid} >> ./log/pid.log
-date >> ./log/pid.log
 echo "runserver.py started at pid: "${pid}
+
+#删除3天前的日志
+find $log -mtime +3 -name "*.log" -exec rm -rf {} \;
