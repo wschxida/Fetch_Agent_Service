8fb8df97d31deb17933b536705abc8d72605e4dd
DELL <wschxida@gmail.com>
2020-12-10 16:20:50 +0800

    11.5 run_in_linux.sh

--- run_in_linux.sh
+++ run_in_linux.sh
@@ -1,12 +1,12 @@
-pid=`ps -ef|grep "gunicorn"| grep -v "grep"|awk '{print $2}'`
+pid=`ps -ef|grep "gunicorn -w 10 -b 127.0.0.1:5101 run:app"| grep -v "grep"|awk '{print $2}'`
 if [ "$pid" != "" ]
 then
         echo "runserver.py already run, stop it first"
         kill -9 ${pid}
 fi
 echo "starting now.."
-nohup gunicorn -w 10 -b 0.0.0.0:5100 run:app > ./log/myout.log 2>&1 &
-pid=`ps -ef|grep "gunicorn -w 10 -b 0.0.0.0:5100 run:app"| grep -v "grep"|awk '{print $2}'`
+nohup gunicorn -w 10 -b 127.0.0.1:5101 run:app > ./log/myout.log 2>&1 &
+pid=`ps -ef|grep "gunicorn -w 10 -b 127.0.0.1:5101 run:app"| grep -v "grep"|awk '{print $2}'`
 echo ${pid} >> ./log/pid.log
 date >> ./log/pid.log
 echo "runserver.py started at pid: "${pid}
