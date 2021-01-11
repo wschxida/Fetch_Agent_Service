a16d7f1a34a7e50378fdccd8cc83f221ec0bdb7e
DELL <wschxida@gmail.com>
2021-01-11 17:00:03 +0800

    11.9 telegram message 返回非法账号提示

---
+++ service_app/model/twitter/extractor/lib/get_username_from_id.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Cedar
+# @Date  : 2021/1/11
+# @Desc  :
+
+
+import requests
+
+
+def username_to_id_facebook(id):
+    url = 'https://www.facebook.com/153080620724'
+    result = 'https://www.facebook.com/DonaldTrump/'
+    return result
+
+
+def username_to_id_twitter(id):
+    url = 'https://twitter.com/intent/user?user_id=2359926157'
+    result = 'https://twitter.com/intent/user?screen_name=CIA'
+    return result
