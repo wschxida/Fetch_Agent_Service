1425d4d716493e699a5ee7ed889a5931163953ea
DELL <wschxida@gmail.com>
2021-01-27 15:14:09 +0800

    13.3 改进gunicorn，config

--- run_in_linux.sh
+++ run_in_linux.sh
@@ -4,15 +4,15 @@ log=/home/kismanager/KIS/Fetch_Agent_Service/log
 time=$(date "+%Y%m%d")
 
 cd $service
-pid=`ps -ef|grep "gunicorn -w 10 -b 127.0.0.1:5101 run:app"| grep -v "grep"|awk '{print $2}'`
+pid=`ps -ef|grep "gunicorn -c gunicorn_fetch_agent.conf run:app"| grep -v "grep"|awk '{print $2}'`
 if [ "$pid" != "" ]
 then
         echo "runserver.py already run, stop it first"
         kill -9 ${pid}
 fi
 echo "starting now.."
-nohup /usr/local/bin/gunicorn -w 10 -b 127.0.0.1:5101 run:app >> $log/Fetch_Agent_Service_$time.log 2>&1 &
-pid=`ps -ef|grep "gunicorn -w 10 -b 127.0.0.1:5101 run:app"| grep -v "grep"|awk '{print $2}'`
+nohup /usr/local/bin/gunicorn -c gunicorn_fetch_agent.conf run:app >> $log/Fetch_Agent_Service_$time.log 2>&1 &
+pid=`ps -ef|grep "gunicorn -c gunicorn_fetch_agent.conf run:app"| grep -v "grep"|awk '{print $2}'`
 echo "runserver.py started at pid: "${pid}
 
 #删除3天前的日志
